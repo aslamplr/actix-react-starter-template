f0c13bf39c4b6b684806387e0483b8e752c5d467
Aslam Ahammed <aslamplr@users.noreply.github.com>
2022-05-05 19:38:03 +0000

    Add cors support

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,7 +4,10 @@ mod services;
 
 use std::env;
 
-use actix_web::{error, get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{
+    error, get, http::header, middleware, web, App, HttpResponse, HttpServer, Responder,
+};
 use env_logger::Env;
 
 use entity::sea_orm;
@@ -38,11 +41,16 @@ async fn hello(_: web::Data<AppContext>) -> impl Responder {
 }
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     dotenv::dotenv().ok();
+    let bind_address: std::net::SocketAddr = env::var("BIND_ADDRESS")
+        .expect("BIND_ADDRESS is not set in .env file")
+        .parse()?;
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let cors_allowed_origin: String =
+        env::var("CORS_ALLOWED_ORIGIN").expect("CORS_ALLOWED_ORIGIN is not in .env file");
 
     let conn = sea_orm::Database::connect(&db_url).await.unwrap();
     Migrator::up(&conn, None).await.unwrap();
@@ -60,11 +68,25 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(json_config)
             .app_data(web::Data::new(state.clone()))
+            .wrap(
+                Cors::default()
+                    .allowed_origin(&cors_allowed_origin)
+                    .allow_any_method()
+                    .allowed_headers(vec![
+                        header::AUTHORIZATION,
+                        header::ACCEPT,
+                        header::CONTENT_TYPE,
+                    ])
+                    .supports_credentials()
+                    .max_age(3600),
+            )
             .wrap(middleware::Logger::default())
             .wrap(middleware::Compress::default())
             .service(web::scope("/api").service(hello).configure(cake_config))
     })
-    .bind(("0.0.0.0", 8000))?
+    .bind(bind_address)?
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
--- app/src/api.ts
+++ app/src/api.ts
@@ -1,5 +1,12 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+
+export const apiFetch = (urlPath: string) =>
+  fetch(
+    `${API_BASE_URL}${urlPath.charAt(0) === '/' ? urlPath : '/' + urlPath}`
+  );
+
 export const fetchApiStatus = async (): Promise<string> => {
-  const reps = await fetch('/api/status');
+  const reps = await apiFetch('/api/status');
   const { msg } = await reps.json();
   return msg;
 };
