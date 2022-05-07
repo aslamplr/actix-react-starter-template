eceb77d5aefa62365032fdedd667583c1fc8c193
Aslam Ahammed <aslamplr@users.noreply.github.com>
2022-05-07 18:19:02 +0000

    Add configurable API_BASE_PATH env

--- app/src/api.ts
+++ app/src/api.ts
@@ -1,8 +1,11 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH;
 
 export const apiFetch = (urlPath: string) =>
   fetch(
-    `${API_BASE_URL}${urlPath.charAt(0) === '/' ? urlPath : '/' + urlPath}`
+    `${API_BASE_URL}${API_BASE_PATH || ''}${
+      urlPath.charAt(0) === '/' ? urlPath : '/' + urlPath
+    }`
   );
 
 export const fetchApiStatus = async (): Promise<string> => {
